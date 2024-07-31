471d75fb5b2dea5a3a33ffaf03a0d24db71f9af2
Luciano E. Fontana <lfontana@dsyr.com.ar>
2024-07-31 13:33:52 -0300

    netlify testing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://webkfc.neotelecd.com/neoapi/webservice.asmx/:path*'
-        }
-      ];
-    },
-  };
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://webkfc.neotelecd.com/neoapi/webservice.asmx/:path*'
+      }
+    ];
+  },
+  exportTrailingSlash: true, 
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'out', 
+};
